4b18eb15dee6ab2d9dd5f3050fa6b4fef30ecbef
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-29 13:13:02 +0530

    Added input validation for GCD calculation

--- greatest comman factor.c
+++ greatest comman factor.c
@@ -4,16 +4,21 @@ int main(){
     int a,b,c;
     printf("Enter two numbers: ");
     scanf("%d %d",&a,&b);
+if(a<b){
+int C=a;
+a=b;
+b=C;
+}
     c=gcd(a,b);
     printf("GCD of %d and %d is %d\n",a,b,c);
     return 0;
 }
 int gcd(int a,int b){
-    int t;
+   
     if(b==0){
         return a;
     }
     else 
+    {
         return gcd(b,a%b);
-   
-}
+    }
